cb748d624324c3a8c993b218b5223c5ea762b45b
James Elias <james.elias95@gmail.com>
2020-08-22 15:54:14 -0700

    Finalized Login Screen
    * Added callback to login screen
    * Added logic to authenticate user
    * Added loaded even to start callback thread

--- RealTimeChatApp/LoginScreen.xaml.cs
+++ RealTimeChatApp/LoginScreen.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,56 @@ namespace RealTimeChatApp
     /// </summary>
     public partial class LoginScreen : Window
     {
+        // Boolean tracking if a user is authorized
+        public bool isAuthorized;
+
+        // Constructor for login screen
         public LoginScreen()
         {
+            isAuthorized = false;
             InitializeComponent();
+        }
+
+        // Callback method that displays authorization notice
+        private void DisplayAuthorizationMessage()
+        {
+            while (!isAuthorized)
+            {
+                Thread.Sleep(500);
+            }
+
+            this.Dispatcher.Invoke(() =>
+            {
+                    MessageBox.Show("User is authorized!");
+            });
+        }
+
+        // Method to validate usernamen and password
+        private bool IsAuthorizedUser(string username, string password)
+        {
+            return username == "User" && password == "Password";
+        }
+
+        // Method called by button click that lets authorized users into the app
+        private void AttemptUserAuthentication(object sender, RoutedEventArgs e)
+        {
+            isAuthorized = IsAuthorizedUser(inputUsername.Text, inputPassword.Password);
+            if (isAuthorized)
+            {
+                MainWindow generalChat = new MainWindow();
+                generalChat.Show();
+                this.Close();
+            } else
+            {
+                MessageBox.Show("Username/password combination not found");
+            }
+        }
+
+        // Background listener thread created on screen load
+        private void LoginLoaded(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(this.DisplayAuthorizationMessage));
+            thread.Start();
         }
     }
 }
