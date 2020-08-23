6461a1ae8ff47d68bb48e645fab69f65ce484209
James Elias <james.elias95@gmail.com>
2020-08-23 12:32:52 -0700

    ChatBox Checkpoint
    * Uploading progress checkpoint on chatbox
    * Created chatbox component
    * Created basic layout of main window

--- RealTimeChatApp/MainWindow.xaml.cs
+++ RealTimeChatApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace RealTimeChatApp
         {
             InitializeComponent();
         }
+
+        private void UserList_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
